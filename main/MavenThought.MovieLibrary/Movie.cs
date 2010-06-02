72e6d5cfbeb5f44d16c20b0c5b4f1bfd5f38b28c
Amir Barylko <amir@barylko.com>
2010-06-02 09:08:09 -0500

    Added Movie to the project

---
+++ main/MavenThought.MovieLibrary/Movie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MavenThought.MovieLibrary
+{
+    /// <summary>
+    /// Implementation for movie
+    /// </summary>
+    public class Movie : IMovie
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="releaseDate"></param>
+        public Movie(string title, DateTime? releaseDate)
+        {
+            this.Title = title;
+            this.ReleaseDate = releaseDate;
+        }
+
+        /// <summary>
+        /// Title of the movie
+        /// </summary>
+        public string Title
+        {
+            get; private set;
+        }
+
+        /// <summary>
+        /// Release date of the movie
+        /// </summary>
+        public DateTime? ReleaseDate
+        {
+            get; private set;
+        }
+    }
+}
